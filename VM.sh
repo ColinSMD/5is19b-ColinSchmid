49301ae35971cce55385bd3c374f768935942aae
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-03-25 10:48:45 +0100

    Create VM.sh

---
+++ VM.sh
@@ -0,0 +1,6 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
+tar xvfz node_exporter-*.*-amd64.tar.gz
+sudo mv node_exporter-*.*-amd64/node_exporter /usr/local/bin/
+
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
