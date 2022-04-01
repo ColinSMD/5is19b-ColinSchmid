203f5eb3e977de45a7717e50107f5c9fe3989b04
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-04-01 08:01:16 +0200

    Update VM.sh

--- VM.sh
+++ VM.sh
@@ -1,6 +1,7 @@
-wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
-tar xvfz node_exporter-*.*-amd64.tar.gz
-sudo mv node_exporter-*.*-amd64/node_exporter /usr/local/bin/
+sudo apt install prometheus-node-exporter
 
-sudo systemctl daemon-reload
-sudo systemctl start node_exporter
+sudo service node-exporter status
+
+sudo service node-exporter stop
+sudo service node-exporter start
+sudo service node-exporter restart
