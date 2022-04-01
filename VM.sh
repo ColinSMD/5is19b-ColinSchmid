b98293fadcc2b7febc9081590c68b7ee594a8bcc
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-04-01 09:41:21 +0200

    Update VM.sh

--- VM.sh
+++ VM.sh
@@ -1,6 +1,6 @@
 sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
-tar xvfz node_exporter-*.*-amd64.tar.gz
-sudo mv node_exporter-*.*-amd64/node_exporter /usr/local/bin/
+tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
+sudo mv node_exporter-1.3.1.linux-amd64.tar.gz /usr/local/bin/
 
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
