72e505eb280cfacaff1c78b70a396cf0ea126ede
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-04-08 08:19:43 +0200

    Update VM.sh

--- VM.sh
+++ VM.sh
@@ -1,6 +1,21 @@
-sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+#!/bin/bash
+cd ~
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
-sudo mv node_exporter-1.3.1.linux-amd64.tar.gz /usr/local/bin/
-
+sudo mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
+sudo useradd -rs /bin/false node_exporter
+sudo tee /etc/systemd/system/node_exporter.service<<EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
