0704e316577af8101304017b4e35b6bce3e54d1a
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-04-08 16:09:36 +0200

    Update VM.sh

--- VM.sh
+++ VM.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 cd ~
-
-
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
 sudo mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
