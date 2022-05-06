02b36da5baa605ccc4cbc0c9f7d6f5029fa7a65a
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-05-06 15:02:31 +0200

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 cd ~
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
+sudo mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
+sudo useradd -rs /bin/false node_exporter
+sudo tee /etc/systemd/system/node_exporter.service<<EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
 
-docker pull prom/node-exporter
-docker run --name node-exporter-container prom/node-exporter
 docker pull prom/prometheus
 docker run --name prometheus-container prom/prometheus
 docker run pull grafana/grafana
