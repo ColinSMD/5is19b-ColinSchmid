edb509f0f7889f4b36f10fc3dcf2c6f3ea7fd601
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-05-06 09:50:10 +0200

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd ~
+
+docker pull prom/node-exporter
+docker pull prom/prometheus
+docker run pull grafana/grafana
 docker run --name node-exporter-container prom/node-exporter
 docker run --name prometheus-container prom/prometheus
 docker run --name grafana-container grafana/grafana
