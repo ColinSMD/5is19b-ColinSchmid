135fe47bf19e5ce02ce8c83422a0dacd2bb2db95
ColinSMD <86480068+ColinSMD@users.noreply.github.com>
2022-06-22 09:21:17 +0200

    Update BOINC.sh

--- BOINC.sh
+++ BOINC.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd ~
 sudo aptitude install boinc-client
+wget https://raw.githubusercontent.com/ColinSMD/5is19b-ColinSchmid/main/boinc_7.4.22_x86_64-pc-linux-gnu.sh -O /opt/boinc_7.4.22_x86_64-pc-linux-gnu.sh
+chmod +x boinc_7.4.22_x86_64-pc-linux-gnu.sh
+sh boinc_7.4.22_x86_64-pc-linux-gnu.sh
